3e25d919031f31746427153171a07cda9075a1ec
hasnainyaqub <hasnainyaqubakm@gmail.com>
2025-12-17 17:33:46 +0500

    OutPut_Parser_GenAI

--- 03_Langchain_models/02.Chat_Models/3.chatmodel_google.py
+++ 03_Langchain_models/02.Chat_Models/3.chatmodel_google.py
@@ -2,6 +2,6 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
-chat_model = ChatGoogleGenerativeAI(model="gemini-pro")
+chat_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 response = chat_model.predict("Write a poem about the sea.")
 print(response)
---
+++ 06_Output_Parsers_in_LangChain/str_output.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatGroq(model="llama-3.3-70b-versatile")  # or other Groq models
+
+# 1st prompt -> detailed report
+template1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+# 2nd prompt -> summary
+template2 = PromptTemplate(
+    template='Write a 5 line summary on the following text. /n {text}',
+    input_variables=['text']
+)
+
+prompt1 = template1.invoke({'topic':'black hole'})
+
+result = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text':result.content})
+
+result1 = model.invoke(prompt2)
+
+print(result1.content)
